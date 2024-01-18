3a0fe30dea3cdcde61deaf0a9f8aba2ae42e7e54
hossein javadi <hosseinj13@yahoo.com>
2024-01-18 12:05:33 +0330

    create connection package and JdbcConnection.java to this package

--- src/main/java/connection/JdbcConnection.java
+++ src/main/java/connection/JdbcConnection.java
@@ -1,4 +1,25 @@
 package connection;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class JdbcConnection {
+
+    private static final Connection connection;
+
+    static {
+        try {
+            connection = DriverManager.getConnection("jdbc:postgresql://localhost:5432/maktab", "postgres", "h9311240246N");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public JdbcConnection() {
+    }
+
+    static public Connection getConnection() {
+        return connection;
+    }
 }
